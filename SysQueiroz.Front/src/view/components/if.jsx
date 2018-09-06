41a41474de2f909e5e4b15d9324a7299de67d081
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2018-09-06 17:47:21 -0300

    preparando evolução do If para ter o Else e controle de acesso

--- SysQueiroz.Front/src/view/components/if.jsx
+++ SysQueiroz.Front/src/view/components/if.jsx
@@ -1,4 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+// export default class If extends Component {
+
+//     render() {
+// debugger
+//         const { children, condition } = this.props
+
+//         const lastIndex = children.length - 1
+//         const ifElse = children[lastIndex]._self.constructor.name === 'Else'
+//         const contentIf = ifElse ? children.splice(lastIndex, 1) : children
+//         const contentElse = ifElse ? children[lastIndex] : false
+
+//         if (condition) {
+//             return <div>{contentIf}</div>
+//         } else {
+//             return <div>{contentElse.children}</div>
+//         }
+//     }
+// }
+
+// export class Else extends Component {    
+//     render = () => <div>{this.props.children}</div>
+// }
 
 export default props => {
     if (props.condition) {
