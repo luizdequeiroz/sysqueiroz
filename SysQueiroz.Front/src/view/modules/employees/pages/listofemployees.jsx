1a50da75eacec46a6977e0efc3628ff9027ffd82
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2018-10-02 18:51:16 -0300

    alias que não estava sendo usado em list of employees.

--- SysQueiroz.Front/src/view/modules/employees/pages/listofemployees.jsx
+++ SysQueiroz.Front/src/view/modules/employees/pages/listofemployees.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import BootstrapTable from 'react-bootstrap-table-next'
-import { employeesdepartmant, employee } from '../../../../data/alias/keys'
+import { employeesdepartmant } from '../../../../data/alias/keys'
 import { requestToReducer, showModal, closeModal } from '../../../../data/dispatchers'
 
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit'
